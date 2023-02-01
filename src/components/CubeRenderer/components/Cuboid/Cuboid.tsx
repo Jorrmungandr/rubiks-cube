5858f29256767d66ed0a0d7820535be35b539f22
Edgar Marques <edgarmarques.110@gmail.com>
2023-02-01 02:34:44 -0300

    feat: add base for cube rotation

--- src/components/CubeRenderer/components/Cuboid/Cuboid.tsx
+++ src/components/CubeRenderer/components/Cuboid/Cuboid.tsx
@@ -1,3 +1,4 @@
+import { useFrame } from '@react-three/fiber';
 import { useRef } from 'react';
 import { Mesh } from 'three';
 import { faceColors } from '../../../../variables';
@@ -9,6 +10,22 @@ type CuboidProps = {
 
 export function Cuboid({ position, colorIndexes }: CuboidProps) {
   const mesh = useRef<Mesh>(null!);
+
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+    const speedMultiplier = 1;
+
+    if (elapsedTime > (Math.PI * 2) / speedMultiplier) return;
+
+    const distance = Math.sqrt(position[1] ** 2 + position[2] ** 2);
+
+    mesh.current.rotation.x = elapsedTime * speedMultiplier;
+
+    const cuboidAngleInRadians = Math.atan2(position[2], position[1]);
+
+    mesh.current.position.y = Math.cos((elapsedTime + cuboidAngleInRadians) * speedMultiplier) * distance;
+    mesh.current.position.z = Math.sin((elapsedTime + cuboidAngleInRadians) * speedMultiplier) * distance;
+  });
 
   return (
     <mesh ref={mesh} position={position}>
